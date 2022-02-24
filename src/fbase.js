ce64f929c919a62affa7502d39087a9cf47239cc
박주강 <jukang@bagjugang-ui-Macmini.local>
2022-02-24 13:12:59 +0900

    write collection

--- src/fbase.js
+++ src/fbase.js
@@ -1,18 +1,20 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import "firebase/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
   appId: process.env.REACT_APP_APP_ID,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
-
 firebase.initializeApp(firebaseConfig);
+console.log(firebase);
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
 const Home = () => {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log("저장될 트윗", nweet);
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="what's on your mind?"
+        maxLength={120}
+        onChange={onChange}
+      />
+      <input type="submit" value="Nweet" />
+    </form>
+  );
 };
 
 export default Home;
